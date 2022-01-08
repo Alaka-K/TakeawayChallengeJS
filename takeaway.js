9fd5e062c20c14290e978b8cfffe2f6ebac22122
kehinde alaka <kennyalaka93@gmail.com>
2022-01-08 21:15:38 +0000

    Added addOrder method but test is failing. However it is working in node. not sure what is wring with the test

--- takeaway.js
+++ takeaway.js
@@ -3,10 +3,15 @@ const Menu = require('./menu');
 class Takeaway {
     constructor () {
       this.orders = []
+
     }
 
     getOrders () {
-      return this.orders
+    return this.orders
+    }
+
+    addOrder (item) {
+      return this.orders.push(item);
     }
 }
 
--- takeaway.test.js
+++ takeaway.test.js
@@ -1,7 +1,14 @@
 const Takeaway = require('./takeaway');
+
 describe('Takeaway', () => {
   it('initialize with empty orders array', () => {
     takeaway = new Takeaway();
     expect(takeaway.getOrders()).toStrictEqual([]);
   });
-});
+
+  it('add an item to order', () => {
+    takeaway = new Takeaway();
+    addOrder('Rice');
+    expect(takeaway.getOrders().length).toEqual(1);
+  });
+}); 
